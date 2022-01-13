70c0d4005635f6e7a202b138868ba9b514c3eb23
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2022-01-13 20:36:56 +0300

    test

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,8 +1,9 @@
 package com.example.service;
 
+import com.example.dto.UserDto;
 import com.example.entity.UserEntity;
 
 public interface UserService {
 
-    UserEntity registerUser()
+    UserEntity registerUser(UserDto userDto);
 }
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,2 +1,15 @@
-package com.example.service.impl;public class UserServiceImpl {
+package com.example.service.impl;
+
+import com.example.dto.UserDto;
+import com.example.entity.UserEntity;
+import com.example.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserEntity registerUser(UserDto userDto) {
+        return null;
+    }
 }
